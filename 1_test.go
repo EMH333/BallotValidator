4db2a7cbf99fb8c73b570f7f472fb717d9e310db
Ethan Hampton <etho@ethohampton.com>
2022-02-15 15:30:17 -0800

    Fix tests

--- 1_test.go
+++ 1_test.go
@@ -10,13 +10,13 @@ func TestStepOne(t *testing.T) {
 	var validVotersUndergraduate []string = []string{"789", "012"}
 	var validVotersUndefined []string = []string{"345", "678"}
 	var votes []Vote = []Vote{
-		{[]string{"123", "456", "789"}, time.Now(), "123"},
-		{[]string{"012", "123", "456"}, time.Now(), "056"}, //invalid
-		{[]string{"456", "789", "012"}, time.Now(), "456"},
-		{[]string{"012", "123", "456"}, time.Now(), "000"}, //invalid
-		{[]string{"789", "012", "123"}, time.Now(), "789"},
-		{[]string{"012", "123", "456"}, time.Now(), "012"},
-		{[]string{"012", "123", "456"}, time.Now(), "345"},
+		{[]string{"123", "456", "789"}, time.Now(), "123", ""},
+		{[]string{"012", "123", "456"}, time.Now(), "056", ""}, //invalid
+		{[]string{"456", "789", "012"}, time.Now(), "456", ""},
+		{[]string{"012", "123", "456"}, time.Now(), "000", ""}, //invalid
+		{[]string{"789", "012", "123"}, time.Now(), "789", ""},
+		{[]string{"012", "123", "456"}, time.Now(), "012", ""},
+		{[]string{"012", "123", "456"}, time.Now(), "345", ""},
 	}
 
 	valid, invalid, _ := stepOne(votes, &validVotersGraduate, &validVotersUndergraduate, &validVotersUndefined)
