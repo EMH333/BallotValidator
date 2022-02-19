c4103eafd50fb5a58197cf1b61d48dc0e75ea95a
Ethan Hampton <etho@ethohampton.com>
2022-02-19 12:58:12 -0800

    Move all file operations to fileOps

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 
 	"ethohampton.com/BallotCleaner/internal/steps"
 	"ethohampton.com/BallotCleaner/internal/util"
@@ -60,7 +55,7 @@ func main() {
 	var dayToDayFormat = fmt.Sprint(startDay) + "-" + fmt.Sprint(endDay)
 
 	//"random" seed so winners are deterministic
-	var seed = loadSeed() + "-" + dayToDayFormat //include days in picking so it is unique
+	var seed = util.LoadSeed() + "-" + dayToDayFormat //include days in picking so it is unique
 
 	_, err := os.Stat("output")
 	if os.IsNotExist(err) && os.Mkdir("output", 0755) != nil {
@@ -69,9 +64,9 @@ func main() {
 
 	// Load the valid voters
 	log.Println("Loading valid voters...")
-	validVotersGraduate = loadValidVoters("data/validVoters.csv", "G")
-	validVotersUndergrad = loadValidVoters("data/validVoters.csv", "UG")
-	validVotersUndefined = loadValidVoters("data/validVoters.csv", "Self Identified on Ballot")
+	validVotersGraduate = util.LoadValidVoters("data/validVoters.csv", "G")
+	validVotersUndergrad = util.LoadValidVoters("data/validVoters.csv", "UG")
+	validVotersUndefined = util.LoadValidVoters("data/validVoters.csv", "Self Identified on Ballot")
 
 	log.Printf("There are %d valid voters for graduate students\n", len(validVotersGraduate))
 	log.Printf("There are %d valid voters for undergrad students\n", len(validVotersUndergrad))
@@ -79,7 +74,7 @@ func main() {
 
 	// Load the already voted
 	log.Printf("Loading already voted up to day %d...\n", startDay)
-	alreadyVotedPrevious = loadAlreadyVoted("alreadyVoted", int64(startDay))
+	alreadyVotedPrevious = util.LoadAlreadyVoted("alreadyVoted", int64(startDay))
 
 	log.Printf("%d students have already voted\n", len(alreadyVotedPrevious))
 
@@ -92,9 +87,9 @@ func main() {
 	log.Println()
 	log.Println("Step 1: Valid voter")
 	validPostOne, invalidPostOne, oneSummary := steps.StepOne(votes, &validVotersGraduate, &validVotersUndergrad, &validVotersUndefined)
-	storeVotes(validPostOne, "1-valid-"+dayToDayFormat+".csv")
-	storeVotes(invalidPostOne, "1-invalid-"+dayToDayFormat+".csv")
-	storeSummary(oneSummary, "1-summary-"+dayToDayFormat+".txt")
+	util.StoreVotes(validPostOne, "1-valid-"+dayToDayFormat+".csv")
+	util.StoreVotes(invalidPostOne, "1-invalid-"+dayToDayFormat+".csv")
+	util.StoreSummary(oneSummary, "1-summary-"+dayToDayFormat+".txt")
 	log.Println("Step 1: Invalid votes:", oneSummary.Invalid)
 	log.Println("Step 1: Valid votes:", oneSummary.Valid)
 
@@ -102,10 +97,10 @@ func main() {
 	log.Println()
 	log.Println("Step 2: Dedupe")
 	validPostTwo, invalidPostTwo, alreadyVotedToday, twoSummary := steps.StepTwo(validPostOne, &alreadyVotedPrevious)
-	storeVotes(validPostTwo, "2-valid-"+dayToDayFormat+".csv")
-	storeVotes(invalidPostTwo, "2-invalid-"+dayToDayFormat+".csv")
-	storeSummary(twoSummary, "2-summary-"+dayToDayFormat+".txt")
-	storeAlreadyVoted(alreadyVotedToday, "alreadyVoted-"+dayToDayFormat+".csv")
+	util.StoreVotes(validPostTwo, "2-valid-"+dayToDayFormat+".csv")
+	util.StoreVotes(invalidPostTwo, "2-invalid-"+dayToDayFormat+".csv")
+	util.StoreSummary(twoSummary, "2-summary-"+dayToDayFormat+".txt")
+	util.StoreAlreadyVoted(alreadyVotedToday, "alreadyVoted-"+dayToDayFormat+".csv")
 	log.Println("Step 2: Invalid votes:", twoSummary.Invalid)
 	log.Println("Step 2: Valid votes:", twoSummary.Valid)
 
@@ -113,9 +108,9 @@ func main() {
 	log.Println()
 	log.Println("Step 3: Grad/undergrad")
 	validPostThree, invalidPostThree, threeSummary := steps.StepThree(validPostTwo, &validVotersGraduate, &validVotersUndergrad)
-	storeVotes(validPostThree, "3-valid-"+dayToDayFormat+".csv")
-	storeVotes(invalidPostThree, "3-modified-"+dayToDayFormat+".csv")
-	storeSummary(threeSummary, "3-summary-"+dayToDayFormat+".txt")
+	util.StoreVotes(validPostThree, "3-valid-"+dayToDayFormat+".csv")
+	util.StoreVotes(invalidPostThree, "3-modified-"+dayToDayFormat+".csv")
+	util.StoreSummary(threeSummary, "3-summary-"+dayToDayFormat+".txt")
 	log.Println("Step 3: Modified votes:", threeSummary.Invalid)
 	log.Println("Step 3: Valid votes:", threeSummary.Valid)
 
@@ -123,185 +118,12 @@ func main() {
 	log.Println()
 	log.Println("Step 4: Incentives")
 	postFour, winners, fourSummary := steps.StepFour(validPostThree, seed, numToPick)
-	storeVotes(postFour, "4-valid-"+dayToDayFormat+".csv")
-	storeSummary(fourSummary, "4-summary-"+dayToDayFormat+".txt")
-	storeAlreadyVoted(winners, "incentive-winners-"+dayToDayFormat+".csv")
+	util.StoreVotes(postFour, "4-valid-"+dayToDayFormat+".csv")
+	util.StoreSummary(fourSummary, "4-summary-"+dayToDayFormat+".txt")
+	util.StoreAlreadyVoted(winners, "incentive-winners-"+dayToDayFormat+".csv")
 	log.Println("Step 4: Valid votes:", threeSummary.Valid)
 	log.Println("Step 4: Selected winners:", len(winners))
 
 	//experimental
 	steps.StepFourtyTwo(postFour, "output/results")
-}
-
-const VALID_STATUS = 4
-const VALID_ONID_EMAIL = 2
-
-func loadValidVoters(fileName string, indicator string) []string {
-	var voters []string
-
-	//open csv file
-	f, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	//with modifications to handle the specifics of the valid votes list
-	csvReader := csv.NewReader(f)
-	csvReader.Comma = '\t'
-	csvReader.TrimLeadingSpace = true
-
-	for {
-		rec, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		//check and see if the indicator (G_UG_STATUS) is valid for who we are trying to process
-		if rec[VALID_STATUS] == indicator {
-			voters = append(voters, rec[VALID_ONID_EMAIL])
-		}
-	}
-
-	return voters
-}
-
-func loadAlreadyVoted(folderName string, upToDay int64) []string {
-	var alreadyVoted []string
-
-	var folder = "data/" + folderName + "/"
-
-	_, err := os.Stat(folder)
-	if os.IsNotExist(err) {
-		log.Fatal("alreadyVoted doesn't exist within data")
-	}
-
-	files, err := ioutil.ReadDir(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, file := range files {
-		fileDay, err := strconv.ParseInt(strings.Split(file.Name(), "-")[1], 10, 64) //expecting alreadyVoted-<day>-<endDay>.csv
-		if err != nil {
-			log.Fatalln("Already voted file name formated incorrectly", err)
-		}
-
-		// ignore files that are past today since they won't yield helpful results
-		if fileDay >= upToDay {
-			continue
-		}
-
-		file, err := os.Open(folder + file.Name())
-		if err != nil {
-			log.Fatalln("Error opening already voted file", err)
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			alreadyVoted = append(alreadyVoted, scanner.Text())
-		}
-	}
-
-	alreadyVoted = util.RemoveDuplicateStr(alreadyVoted) //make sure we don't have any duplicates (though it doesn't really matter)
-
-	return alreadyVoted
-}
-
-func storeVotes(votes []util.Vote, filename string) {
-	//store the vote.raw in csv format under filename
-	f, err := os.Create("output/" + filename)
-	if err != nil {
-		log.Fatalln("failed to open file", err)
-	}
-	defer f.Close()
-
-	w := csv.NewWriter(f)
-	defer w.Flush()
-
-	for _, record := range votes {
-		if err := w.Write(record.Raw); err != nil {
-			log.Fatalln("error writing record to file", err)
-		}
-	}
-
-	w.Flush() // make sure we flush before closing file
-}
-
-func storeSummary(summary util.Summary, filename string) {
-	f, err := os.Create("output/" + filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// remember to close the file
-	defer f.Close()
-
-	_, err = f.WriteString(summary.StepInfo + "\n")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = f.WriteString(fmt.Sprintf("Processed: %d\n", summary.Processed))
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = f.WriteString(fmt.Sprintf("Valid: %d\n", summary.Valid))
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = f.WriteString(fmt.Sprintf("Invalid: %d\n", summary.Invalid))
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(summary.Log) != 0 {
-		_, err = f.WriteString("\n\nLog Messages:\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, message := range summary.Log {
-			_, err = f.WriteString(message + "\n")
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-}
-
-func storeAlreadyVoted(alreadyVoted []string, filename string) {
-	f, err := os.Create("output/" + filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// remember to close the file
-	defer f.Close()
-
-	for _, record := range alreadyVoted {
-		_, err = f.WriteString(record + "\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func loadSeed() string {
-	//load the seed from the seed.txt file
-	f, err := os.Open("data/seed.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	scanner.Scan()
-	seed := scanner.Text()
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	if seed == "" {
-		log.Fatal("seed.txt is empty")
-	}
-	return seed
 }
---
+++ internal/util/fileOps.go
@@ -0,0 +1,270 @@
+package util
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var EPOCH, epochErr = time.Parse("2006-Jan-02 03:04:05", "2022-Feb-14 00:00:01")
+
+// values to use when importing from csv
+const IMPORT_TIMESTAMP = 1 //using end date so it is consistent across submission times
+const IMPORT_ONID = 74
+const IMPORT_COMPLETE = 6
+const IMPORT_ID = 8
+
+func LoadVotesCSV(fileName string, startDay, endDay int64) []Vote {
+	// make sure our epoch is valid
+	if epochErr != nil {
+		log.Fatal(epochErr)
+	}
+
+	var validStartTime = EPOCH.Add(time.Duration(startDay) * 24 * time.Hour)
+	var validEndTime = EPOCH.Add(time.Duration(endDay+1) * 24 * time.Hour) // add one day to end day
+
+	var votes []Vote
+	//return []string{"TODO"}
+	//load csv file
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// read csv values using csv.Reader
+	//with modifications to handle the specifics of the valid votes list
+	csvReader := csv.NewReader(f)
+	csvReader.Comma = ','
+	csvReader.TrimLeadingSpace = true
+
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//skip the first few rows
+		if rec[IMPORT_TIMESTAMP] == "EndDate" || rec[IMPORT_TIMESTAMP] == "End Date" || strings.Contains(rec[IMPORT_TIMESTAMP], "ImportId") {
+			continue
+		}
+
+		timestamp, err := time.Parse("1/2/2006 15:04", rec[IMPORT_TIMESTAMP]) //2/14/2022 9:10
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//make sure it is only reading the correct day
+		if timestamp.Before(validStartTime) || timestamp.After(validEndTime) {
+			continue
+		}
+
+		ONID := rec[IMPORT_ONID]
+		//sanity check to make sure the ONID looks like an email
+		if !strings.Contains(ONID, "@oregonstate.edu") {
+			log.Fatalf("ONID is not an email address: %s\n", ONID)
+		}
+
+		if strings.Contains(strings.Split(ONID, "@")[0], ".") {
+			log.Fatalf("ONID should not contain a dot: %s\n", ONID)
+		}
+
+		//make sure it is a complete row
+		if rec[IMPORT_COMPLETE] != "TRUE" {
+			log.Fatalf("Vote is not complete: %+v\n", rec)
+		}
+
+		id := rec[IMPORT_ID]
+		if !strings.HasPrefix(rec[IMPORT_ID], "R_") {
+			log.Fatalf("Response ID is not valid: %+v\n", rec)
+		}
+
+		//append rec to votes
+		votes = append(votes, Vote{Raw: rec, Timestamp: timestamp, ONID: ONID, ID: id})
+	}
+
+	return votes
+}
+
+const VALID_STATUS = 4
+const VALID_ONID_EMAIL = 2
+
+func LoadValidVoters(fileName string, indicator string) []string {
+	var voters []string
+
+	//open csv file
+	f, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	//with modifications to handle the specifics of the valid votes list
+	csvReader := csv.NewReader(f)
+	csvReader.Comma = '\t'
+	csvReader.TrimLeadingSpace = true
+
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//check and see if the indicator (G_UG_STATUS) is valid for who we are trying to process
+		if rec[VALID_STATUS] == indicator {
+			voters = append(voters, rec[VALID_ONID_EMAIL])
+		}
+	}
+
+	return voters
+}
+
+func LoadAlreadyVoted(folderName string, upToDay int64) []string {
+	var alreadyVoted []string
+
+	var folder = "data/" + folderName + "/"
+
+	_, err := os.Stat(folder)
+	if os.IsNotExist(err) {
+		log.Fatal("alreadyVoted doesn't exist within data")
+	}
+
+	files, err := ioutil.ReadDir(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fileDay, err := strconv.ParseInt(strings.Split(file.Name(), "-")[1], 10, 64) //expecting alreadyVoted-<day>-<endDay>.csv
+		if err != nil {
+			log.Fatalln("Already voted file name formated incorrectly", err)
+		}
+
+		// ignore files that are past today since they won't yield helpful results
+		if fileDay >= upToDay {
+			continue
+		}
+
+		file, err := os.Open(folder + file.Name())
+		if err != nil {
+			log.Fatalln("Error opening already voted file", err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			alreadyVoted = append(alreadyVoted, scanner.Text())
+		}
+	}
+
+	alreadyVoted = RemoveDuplicateStr(alreadyVoted) //make sure we don't have any duplicates (though it doesn't really matter)
+
+	return alreadyVoted
+}
+
+func StoreVotes(votes []Vote, filename string) {
+	//store the vote.raw in csv format under filename
+	f, err := os.Create("output/" + filename)
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	defer w.Flush()
+
+	for _, record := range votes {
+		if err := w.Write(record.Raw); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
+
+	w.Flush() // make sure we flush before closing file
+}
+
+func StoreSummary(summary Summary, filename string) {
+	f, err := os.Create("output/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file
+	defer f.Close()
+
+	_, err = f.WriteString(summary.StepInfo + "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = f.WriteString(fmt.Sprintf("Processed: %d\n", summary.Processed))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = f.WriteString(fmt.Sprintf("Valid: %d\n", summary.Valid))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = f.WriteString(fmt.Sprintf("Invalid: %d\n", summary.Invalid))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(summary.Log) != 0 {
+		_, err = f.WriteString("\n\nLog Messages:\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, message := range summary.Log {
+			_, err = f.WriteString(message + "\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func StoreAlreadyVoted(alreadyVoted []string, filename string) {
+	f, err := os.Create("output/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file
+	defer f.Close()
+
+	for _, record := range alreadyVoted {
+		_, err = f.WriteString(record + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func LoadSeed() string {
+	//load the seed from the seed.txt file
+	f, err := os.Open("data/seed.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	seed := scanner.Text()
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	if seed == "" {
+		log.Fatal("seed.txt is empty")
+	}
+	return seed
+}
