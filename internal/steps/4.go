de1fefdbf9ad82c954dc6c9bc09a9be8f4bf2037
Ethan Hampton <etho@ethohampton.com>
2023-03-04 19:13:03 -0800

    Update incentives to use local random instance

--- internal/steps/4.go
+++ internal/steps/4.go
@@ -3,6 +3,7 @@ package steps
 import (
 	"fmt"
 	"hash/fnv"
+	"log"
 	"math/rand"
 
 	"ethohampton.com/BallotCleaner/internal/util"
@@ -17,12 +18,13 @@ func StepFour(previousVotes []string, votes []util.Vote, seed string, numberToPi
 	var hashAlgo = fnv.New64a()
 	hashAlgo.Write([]byte(seed))
 
-	rand.Seed(int64(hashAlgo.Sum64())) //seed based on the given seed (which should already include the start and end dates)
+	//seed based on the given seed (which should already include the start and end dates)
+	numberGenerator := rand.New(rand.NewSource(int64(hashAlgo.Sum64())))
 
 	for i := 0; i < numberToPick; i++ {
-		//randomVal := rand.Intn(initialSizeCurrentVotes + initialSizePreviousVotes)
-		//randomVal := rand.Intn(initialSizeCurrentVotes) // for the weekend draw
-		randomVal := rand.Intn(initialSizeCurrentVotes + initialSizePreviousVotes + 4) // for the 4 people who tagged us
+		randomVal := numberGenerator.Intn(initialSizeCurrentVotes + initialSizePreviousVotes)
+		//randomVal := numberGenerator.Intn(initialSizeCurrentVotes) // for the weekend draw
+		//randomVal := numberGenerator.Intn(initialSizeCurrentVotes + initialSizePreviousVotes + 4) // for the 4 people who tagged us
 
 		//if the random val is in the current votes, then grab the vote and add it to the winners
 		//otherwise it is in the previous votes, so use that
@@ -43,8 +45,9 @@ func StepFour(previousVotes []string, votes []util.Vote, seed string, numberToPi
 				i-- //try again
 			}
 		} else {
-			winners = append(winners, "tagged "+fmt.Sprint(randomVal-initialSizeCurrentVotes-initialSizePreviousVotes))
-			logMessages = append(logMessages, "Winner: tagged "+fmt.Sprint(randomVal-initialSizeCurrentVotes-initialSizePreviousVotes)+" chosen with random value "+fmt.Sprint(randomVal))
+			log.Fatal("Random value is out of bounds. This should never happen.")
+			//winners = append(winners, "tagged "+fmt.Sprint(randomVal-initialSizeCurrentVotes-initialSizePreviousVotes))
+			//logMessages = append(logMessages, "Winner: tagged "+fmt.Sprint(randomVal-initialSizeCurrentVotes-initialSizePreviousVotes)+" chosen with random value "+fmt.Sprint(randomVal))
 		}
 	}
 
