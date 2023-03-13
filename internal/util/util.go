d8f49d8db5a4b78c3d30e3762733a31c464d5940
Ethan Hampton <etho@ethohampton.com>
2023-03-13 16:06:56 -0700

    Add all senate candidates to write-in catching

--- internal/util/util.go
+++ internal/util/util.go
@@ -45,8 +45,35 @@ func CleanVote(vote string) string {
 	vote = strings.ReplaceAll(vote, "ABUKAR MOHAMMED", "Abukar Mohamed")
 	vote = strings.ReplaceAll(vote, "COLE PETERS", "Cole Peters")
 
+	//for senate, same person
+	vote = strings.ReplaceAll(vote, "DOLA POPOOLA", "Ayodola Kayode-Popoola")
 	//for senate
+	vote = strings.ReplaceAll(vote, "ABHISHEK ENAGUTHI", "Abhishek Enaguthi")
+	vote = strings.ReplaceAll(vote, "ALEXANDER KERNER", "Alexander Kerner")
+	vote = strings.ReplaceAll(vote, "ANTHONY BUTLER-TORREZ", "Anthony Butler-Torrez")
+	vote = strings.ReplaceAll(vote, "AUDREY PORTER", "Audrey Porter")
+	vote = strings.ReplaceAll(vote, "AYODOLA KAYODE-POPOOLA", "Ayodola Kayode-Popoola")
+	vote = strings.ReplaceAll(vote, "CAMRYN LAU", "Camryn Lau")
+	vote = strings.ReplaceAll(vote, "CAROLYN PEARCE", "Carolyn Pearce")
 	vote = strings.ReplaceAll(vote, "CONNOR ROBERTS", "Connor Roberts")
+	vote = strings.ReplaceAll(vote, "ELIZABETH ECKMAN", "Elizabeth Eckman")
+	vote = strings.ReplaceAll(vote, "ERICA NYARKO", "Erica Nyarko")
+	vote = strings.ReplaceAll(vote, "EVAN RUDISILE", "Evan Rudisile")
+	vote = strings.ReplaceAll(vote, "GABRIEL THOMISON", "Gabriel Thomison")
+	vote = strings.ReplaceAll(vote, "HAILEY BROWN", "Hailey Brown")
+	vote = strings.ReplaceAll(vote, "JAMIE HAMLIN", "Jamie Hamlin")
+	vote = strings.ReplaceAll(vote, "KATYAYANI KARLAPATI", "Katyayani Karlapati")
+	vote = strings.ReplaceAll(vote, "MAIA BROWN", "Maia Brown")
+	vote = strings.ReplaceAll(vote, "MARCUS PAUL ANTOLLI", "Marcus Paul Antolli")
+	vote = strings.ReplaceAll(vote, "NATHAN SCHMIDT", "Nathan Schmidt")
+	vote = strings.ReplaceAll(vote, "OLIVIA CARTWRIGHT", "Olivia Cartwright")
+	vote = strings.ReplaceAll(vote, "RHYAN STEPHENSON", "Rhyan Stephenson")
+	vote = strings.ReplaceAll(vote, "RICHARD DAVID DEININGER", "Richard David Deininger")
+	vote = strings.ReplaceAll(vote, "RICHARD PIAZZA", "Richard Piazza")
+	vote = strings.ReplaceAll(vote, "RILEY WALSH", "Riley Walsh")
+	vote = strings.ReplaceAll(vote, "SARAH THEALL", "Sarah Theall")
+	vote = strings.ReplaceAll(vote, "SHAWN DURR", "Shawn Durr")
+	vote = strings.ReplaceAll(vote, "TEAGHAN KNOX", "Teaghan Knox")
 
 	return vote
 }
