ab366f6c55b95c57237f95ef3139febfe84c96e6
Ethan Hampton <etho@ethohampton.com>
2025-12-28 15:40:06 -0600

    More step 3 tests

--- internal/steps/3_test.go
+++ internal/steps/3_test.go
@@ -7,8 +7,8 @@ import (
 	"ethohampton.com/BallotValidator/internal/util"
 )
 
-// TODO create valid test now that we can control valid offsets
-func TestStepThree(t *testing.T) {
+// TODO create valid test for masking vote based on student status now that we can control valid offsets
+func TestStepThreeNoConfig(t *testing.T) {
 	var votes []util.Vote = []util.Vote{
 		{Raw: []string{"123", "456", "789"}, Timestamp: time.Now(), ONID: "abc", ID: ""},
 		{Raw: []string{"456", "789", "012"}, Timestamp: time.Now(), ONID: "efg", ID: ""},
@@ -28,5 +28,51 @@ func TestStepThree(t *testing.T) {
 
 	if len(invalid) != 0 {
 		t.Error("Received invalid voters unexpectedly")
+	}
+}
+
+func TestStepThreeUndergradWrong(t *testing.T) {
+	var votes []util.Vote = []util.Vote{
+		{Raw: []string{"Undergraduate Student", "456", "789"}, Timestamp: time.Now(), ONID: "abc", ID: ""},
+		{Raw: []string{"Graduate Student", "789", "012"}, Timestamp: time.Now(), ONID: "efg", ID: ""},
+		{Raw: []string{"Undergraduate Student", "012", "123"}, Timestamp: time.Now(), ONID: "hij", ID: ""},
+		{Raw: []string{"Undergraduate Student", "123", "456"}, Timestamp: time.Now(), ONID: "lmn", ID: ""},
+		{Raw: []string{"Undergraduate Student", "123", "456"}, Timestamp: time.Now(), ONID: "opq", ID: ""},
+	}
+
+	var validGrad = []string{}
+	var validUndergrad = []string{"abc", "efg", "hij", "lmn", "opq"}
+
+	valid, invalid, _ := StepThree(&util.CountingConfig{StepThreeChoiceIndex: 0}, votes, &validGrad, &validUndergrad)
+
+	if len(valid) != len(votes) {
+		t.Errorf("Valid voters was %d, expected %d", len(valid), len(votes))
+	}
+
+	if len(invalid) != 1 {
+		t.Errorf("Invalid voters was %d, expected 1", len(invalid))
+	}
+}
+
+func TestStepThreeGradWrong(t *testing.T) {
+	var votes []util.Vote = []util.Vote{
+		{Raw: []string{"Graduate Student", "456", "789"}, Timestamp: time.Now(), ONID: "abc", ID: ""},
+		{Raw: []string{"Graduate Student", "789", "012"}, Timestamp: time.Now(), ONID: "efg", ID: ""},
+		{Raw: []string{"Undergraduate Student", "012", "123"}, Timestamp: time.Now(), ONID: "hij", ID: ""},
+		{Raw: []string{"Graduate Student", "123", "456"}, Timestamp: time.Now(), ONID: "lmn", ID: ""},
+		{Raw: []string{"Graduate Student", "123", "456"}, Timestamp: time.Now(), ONID: "opq", ID: ""},
+	}
+
+	var validGrad = []string{"abc", "efg", "hij", "lmn", "opq"}
+	var validUndergrad = []string{}
+
+	valid, invalid, _ := StepThree(&util.CountingConfig{StepThreeChoiceIndex: 0}, votes, &validGrad, &validUndergrad)
+
+	if len(valid) != len(votes) {
+		t.Errorf("Valid voters was %d, expected %d", len(valid), len(votes))
+	}
+
+	if len(invalid) != 1 {
+		t.Errorf("Invalid voters was %d, expected 1", len(invalid))
 	}
 }
